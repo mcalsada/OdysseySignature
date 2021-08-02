e8887a79a1f0311a8492cf30a9ad0a085b11fba8
Matt Calsada <mcalsada@atlas-sys.com>
2021-08-02 11:27:05 -0400

    Add project files.

---
+++ OdysseySignature/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace OdysseySignature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            if (args.Length != 1)
+            {
+                Console.WriteLine($"Error: Invalid arguments");
+                Console.WriteLine($"Expected Usage: OdysseySignature.exe <FileName>");
+                return;
+            }
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"Error: {args[0]} is not a valid file");
+                return;
+            }
+            
+            var fileInfo = new FileInfo(args[0]);
+
+            SHA1CryptoServiceProvider sha1Encryptor = new SHA1CryptoServiceProvider();
+
+            FileStream stream = null;
+
+            try
+            {
+                stream = fileInfo.OpenRead();
+
+                sha1Encryptor.ComputeHash(stream);
+            }
+            finally
+            {
+                stream.Close();
+            }
+
+            byte[] hash = sha1Encryptor.Hash;
+
+            Console.WriteLine($"Signature: {{SHA1}}{Convert.ToBase64String(hash)}");
+        }
+    }
+}
